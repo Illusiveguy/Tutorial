825ce5b64849d4651f8d1f67fc5b5a840f85307d
Illusiveguy <gamerzero99@gmail.com>
2015-01-07 16:50:01 -0500

    Create MasonRickerd1.py

---
+++ MasonRickerd1.py
@@ -0,0 +1,19 @@
+#<Mason>#
+#titles#
+import os
+import sys
+q1=raw_input"What is your name?"
+q2=raw_input"how are you?"
+q3=raw_input"what time is it?"
+q4=raw_input"where are you?"
+q5=raw_input"why are you here?"
+if q1=="Mason":
+  print"HI Mason"
+elif q1=="Darude-Sandstorm"
+  print"Really?"
+if q3=="Adventure Time"
+  print"No."
+  else:
+    print"cool"
+if q4="what" while=="no"
+  print"Just kidding"
